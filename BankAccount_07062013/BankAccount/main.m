660665ede68894ebfc634744faaca7ddcc5a2cb9
hunglv <hunglv@qsoftvietnam.com>
2013-06-07 13:53:38 +0700

    Start BankAccount

---
+++ BankAccount_07062013/BankAccount/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BankAccount
+//
+//  Created by El Nino on 6/7/13.
+//  Copyright (c) 2013 El Nino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
