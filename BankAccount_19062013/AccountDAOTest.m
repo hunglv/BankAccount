15214201c804e06b013f13877bb0e9af6f9ee5c2
hunglv <hunglv@qsoftvietnam.com>
2013-06-24 14:16:27 +0700

    zz

--- BankAccount_19062013/AccountDAOTest.m
+++ BankAccount_19062013/AccountDAOTest.m
@@ -5,9 +5,35 @@
 
 #import "Kiwi.h"
 #import "AccountDAO.h"
+#import <LocalStorage/LocalStorage.h>
+#import "BankAccount.h"
 
 SPEC_BEGIN(AccountDAOSpec)
 describe(@"AccountDAO test", ^{
+    __block DBEnviroment *enviromentTest;
+    __block AccountDAO *sut;
+    
+    beforeEach(^{
+        enviromentTest = [[DBEnviroment alloc] init];
+        sut = [[AccountDAO alloc] init];
+        sut.databasePath = enviromentTest.databasePath;
+    });
+    
+    afterEach(^{
+        enviromentTest = nil;
+        sut = nil;
+    });
+    context(@"Insert new account", ^{
+        it(@"Insert new Account to empty database should be successful", ^{
+            Account *accountWillInsert = [[Account alloc] init];
+            accountWillInsert.accountNumber = @"1234567890";
+            accountWillInsert.balance = @12.25;
+            accountWillInsert.timeStamp = [NSDate date];
+            
+            BOOL returnValue = [sut insertNewAccount:accountWillInsert];
+            [[theValue(returnValue) should] equal:theValue(YES)];
+        });
+    });
         
 });
 SPEC_END
